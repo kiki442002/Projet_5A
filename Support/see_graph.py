9dbff9bddce064b7143d2e1122898ce9a25541f9
Killian <picot.killian@outlook.fr>
2024-11-19 19:55:02 +0100

    Modifier le fichier see_graph.py pour visualiser les données à partir de DSE.TXT au lieu de HANN_COS.TXT

--- Support/see_graph.py
+++ Support/see_graph.py
@@ -1,23 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+sr=16000
 
-def plot_data(filename, title):
+
+def plot_data(filename, title, x_label='Index', y_label='Valeur'):
     # Lire les données du fichier HANN.TXT
     with open(filename, 'r') as file:
         data = file.readlines()
 
     # Convertir les données en une liste de nombres
-    numbers = [int(line.strip()) for line in data]
+    numbers = [float(line.strip()) for line in data]
+
+    #tableau des fréquences
+    frequencies = np.linspace(0, sr / 2, len(numbers))
 
     # Créer un graphique
-    plt.plot(numbers)
+    plt.plot(frequencies,numbers)
     plt.title(title)
-    plt.xlabel('Index')
-    plt.ylabel('Valeur')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     plt.grid(True)
 
     # Afficher le graphique
     plt.show()
 
-plot_data('HANN_COS.TXT', 'Visualisation de la porte de Hanning avec sinus à 1kHz')
+plot_data('DSE.TXT', 'Visualisation d\'une DSE calculé sur une STM32', 'Fréquence (Hz)', 'Amplitude')
